2e7810b6e9e637bafd4468a06aa1866bf0e3fdb5
Raskovan <alexey.katalkin@gmail.com>
2018-01-22 10:30:54 -0500

    Oy Vey

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,38 @@
 class Customer
-  attr_accessor :first_name, :last_name
+  attr_accessor :first_name, :last_name, :review
+  @@all = []
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.all
+    @@all
+  end
+
+  def add_review(restaurant, review)
+    Review.new(self, restaurant, review)
+  end
+
+  def self.all_names
+    Customer.all.map do |customer|
+      "#{customer.first_name} #{customer.last_name}"
+    end
+  end
+
+  def self.find_by_first_name(name)
+    Customer.all.select do |customer|
+      customer.first_name == name
+    end
+
+  end
+
+
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,20 @@
 class Restaurant
   attr_accessor :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def reviews
+    Review.all.select do |review|
+      review.restaurant == self
+    end
   end
 
 end
