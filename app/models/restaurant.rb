fe9139bb088339ebdd5a96218036fa786bd182db
MinesJA <minesja@gmail.com>
2018-01-22 10:20:09 -0500

    completed assignment

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,37 @@
 class Restaurant
+
+  @@all = []
+
   attr_accessor :name
 
   def initialize(name)
     @name = name
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    # given a string of restaurant name, returns the first restaurant that matches
+
+    self.all.find {|restaurant| restaurant.name == name}
+
+  end
+
+  def reviews
+    # returns an array of all reviews for that restaurant
+    Review.all.select {|review| review.restaurant == self}
+  end
+
+
+  def customers
+    # should return all of the customers who have written reviews of that restaurant.
+
+    self.reviews.map {|review| review.customer}
+
   end
 
 end
