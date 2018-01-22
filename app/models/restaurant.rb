6926d95cbf92bbe07c7f8f7efb55d97b9c6ade0a
morgannegagne <morgannegagne@gmail.com>
2018-01-22 10:31:01 -0500

    Done.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,28 @@
 class Restaurant
   attr_accessor :name
 
+  @@all = []
+
   def initialize(name)
-    @name = name
+    @name = name.upcase
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    name = name.strip.upcase
+    self.all.find{|restaurant| restaurant.name == name}
+  end
+
+  def reviews
+    Review.all.select{|review| review.restaurant == self}
+  end
+
+  def customers
+    reviews.map{|review| review.customer}
   end
 
 end
