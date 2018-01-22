5260739bfd908edfe9dbe11e8401889e0474fc8b
krdiamond <github email address>
2018-01-22 10:30:33 -0500

    solved coding challenge

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,17 @@
 class Review
-  
-end
+  @@all = []
 
+  attr_accessor :restaurant, :customer, :content
+
+  def initialize(restaurant, customer, content)
+    @restaurant = restaurant
+    @customer = customer
+    @content = content
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,25 @@
 require_relative '../config/environment.rb'
+require_relative '../app/models/customer.rb'
+require_relative '../app/models/restaurant.rb'
+require_relative '../app/models/review.rb'
 
 def reload
   load 'config/environment.rb'
 end
+
+kristina = Customer.new("Kristina", "Diamond")
+matt = Customer.new("Matt", "Roseman")
+sara = Customer.new("Sarah", "Fensom")
+mattstup = Customer.new("Matt", "Stupin")
+
+fridays = Restaurant.new("Friday's")
+cheesecake = Restaurant.new("Cheesecake Factory")
+okonomi = Restaurant.new("Okonomi")
+
+review1 = kristina.add_review(fridays, "It sucked")
+review2 = matt.add_review(cheesecake, "no good")
+review3 = sara.add_review(okonomi, "I loved it!")
+review4 = kristina.add_review(okonomi, "my fav place")
+review5 = matt.add_review(okonomi, "the service was slow")
 
 Pry.start
